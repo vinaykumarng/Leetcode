f4bf452158147a5bf603a715dd045301120e127c
Vinay Kumar N G <161075606+vinaykumarng@users.noreply.github.com>
2026-08-19 12:28:11 +0530

    Time: 1 ms (99.96%), Space: 94.7 MB (15.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -13,6 +13,27 @@ class Solution {
         }
 
         return profit;
+
+        // brute force approcah
+        // int minInd = 0;
+        // int minEle = Integer.MAX_VALUE;
+        // for(int i = 0 ; i < len ; i++ ){
+        //     if( prices[i] < minEle){
+        //         minEle = prices[i];
+        //         minInd = i;
+        //     }
+        // }
+        // int maxInd = 0;
+        // int maxEle = 0;
+
+        // for(int i = minInd ; i < len ; i++){
+        //     if(prices[i] > maxEle){
+        //         maxEle = prices[i];
+        //         maxInd = i;
+        //     }
+        // }
+
+        // return prices[maxInd] - prices[minInd];
     }
 
 }
